c8d8076bc45e96ebe2ddc4ef7be7c10b1c71a499
samiksha1402 <80887833+samiksha1402@users.noreply.github.com>
2021-11-08 14:01:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function addUser()
 {
-    player1_name = document.getElementById("player1_name").value;
-    player2_name = document.getElementById("player2_name").value;
+    player1_name = document.getElementById("player_1_input").value;
+    player2_name = document.getElementById("player_2_input").value;
 
     localStorage.setItem("player1_name", player1_name);
     localStorage.setItem("player2_name", player2_name);
